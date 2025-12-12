f65cb9a9e3464d7055d4b6b58a86241808c9cafd
Opilane TTHK <opilane@tthk.ee>
2025-12-12 15:09:27 +0200

    veehoidla programm uus digramm

---
+++ Veehoidla/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Veehoidla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kirjuta programm mis küsib kasutajalt kasutajanime - tsükliga, meetodis
+
+            string kasutajanimi = KasutajaNimi();
+
+
+
+
+            //kui on vale kasutajanimi, programm lõppeb kasutajale teavitamisega
+            string correctuser = "siksseven";
+            if (kasutajanimi != correctuser)
+            {
+                Console.WriteLine(kasutajanimi + "programm lõppeb");
+            }
+            else
+            {
+                //kui on õige, küsi veetaseme hetkeseisu komakohalise arvuga - tsüklis
+                double veetase = 0d;
+                Console.WriteLine("Mis on veetaseme hetkeseisu meetrites?");
+                bool errorState = false;
+                do
+                {
+                    Console.WriteLine("Mis on veetase anduril?:");
+                    double andur = double.Parse(Console.ReadLine());
+                    veetase = andur;
+                    if (andur > veetase)
+                    {
+                        errorState = true;
+                        break;
+                    }
+
+
+                }
+                while (veetase < 1.00d && veetase > 4.25d);
+                //kutsu välja eraldi meetod, mille parameetriks on veetase, mis tagastab string-tüüpi andme
+                Hoiatussõnum(veetase, errorState);
+
+            }
+
+
+
+
+
+            //kutsu välja eraldi meetod, mille parameetriks on veetase, mis tagastab string-tüüpi andme
+            //meetod kontrollib 3 vahemikku, ja tagastab teavituse põhinedes sellele kui kõrge vesi on
+            // vahemik 1: 1-2m tase madal
+            // vahemik 2: 2-3.5m tase normaalne
+            // vahemik 3: 3.5-4.25m tase kõrge
+            // kõik muu annab veateate anduri kohta.
+
+
+        }
+
+        private static void Hoiatussõnum(double veetase, bool errorState)
+        {
+            //meetod kontrollib 3 vahemikku, ja tagastab teavituse põhinedes sellele kui kõrge vesi on
+            if (veetase >= 1.00d && veetase < 2.00d)
+            {
+                Console.WriteLine("VEETASE ON MADAL");
+            }
+            else if (veetase >= 2.0d && veetase < 3.5d)
+            {
+                Console.WriteLine("Tase normaalne");
+            }
+            else if (veetase >= 3.5d && veetase < 4.25d)
+            {
+                Console.WriteLine("tase kõrge");
+            }
+            else
+            {
+                errorState = true;
+                Console.WriteLine("* anduri viga, kontroli seadet *");
+            }
+
+
+
+            // vahemik 1: 1-2m tase madal
+            // vahemik 2: 2-3.5m tase normaalne
+            // vahemik 3: 3.5-4.25m tase kõrge
+            // kõik muu annab veateate anduri kohta.
+        }
+
+
+        private static string KasutajaNimi()
+        {
+            string sisestus = "";
+            do
+            {
+                Console.WriteLine("Tere kasutaja, palun sisesta oma kasutaja nimi");
+                sisestus = Console.ReadLine();
+            } while (sisestus == "");
+            return sisestus;
+        }
+    }
+}
---
+++ if else/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere kasutaja, sisesta oma kasutajanimi:");
+            string kasutajaNimi = Console.ReadLine();
+            if (kasutajaNimi != "Nipitiri")
+            {
+                Console.WriteLine("Sa pole regestrerinud");
+
+            }
+            else
+            {
+                Console.WriteLine("Kas sulle meeldib tulbiud?");
+                string vastus = Console.ReadLine();
+                if (vastus != "jah")
+                {
+                    Console.WriteLine("Ei saa kasutajast aru");
+                }
+                else
+                {
+                    Console.WriteLine("Mulle kaaa");
+                }
+                Console.WriteLine("Kasutaja mis on sinu viiimase nädala mängitud tunde?");
+                int kell = int.Parse(Console.ReadLine());
+                if (kell == 0)
+                {
+                    Console.WriteLine("kahju et sul aega mängida pole :(");
+                }
+                else if (kell > 0 && kell < 10)
+                {
+                    Console.WriteLine("Nõõdukas mängija, tubli");
+                }
+                else if (kell > 10 && kell < 20)
+                {
+                    Console.WriteLine("natuke paljuks läheb, kodutööd kannatavad");
+                }
+                else if (kell > 20 && kell < 40)
+                {
+                    Console.WriteLine("liiga palju mängid, kas sa kooliks üldse nii jõuad?");
+                }
+                else if (kell > 40)
+                {
+                    Console.WriteLine("siin peaks juba sekkuma psühholoog.");
+                }
+
+                Console.WriteLine("Kasutaja mis on sinu viimast jälgpallimängu tulemust?");
+                int punkti = int.Parse(Console.ReadLine());
+                if (punkti == 0)
+                {
+                    Console.WriteLine("PUDRUJALG OLED VÄ!?!");
+                }
+                else if (punkti == 1)
+                {
+                    Console.WriteLine("täitsa ok");
+                }
+                else if (punkti == 2)
+                {
+                    Console.WriteLine("no see juba on parem");
+                }
+                else if (punkti == 3)
+                {
+                    Console.WriteLine("Wow, messi");
+                }
+                else if (punkti > 3)
+                {
+                    Console.WriteLine("Ei tunne sellist skoori, see liga suur minu jaoks");
+                }
+
+
+            }
+        }
+    }
+}
